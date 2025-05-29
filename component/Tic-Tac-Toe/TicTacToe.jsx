17828865e0bd4b7b2bd57b71c42c25e3b9bbeeb1
AbcZoe <you@example.com>
2025-05-29 09:28:06 +0800

    修改了井字棋的標題位置和css

--- component/Tic-Tac-Toe/TicTacToe.jsx
+++ component/Tic-Tac-Toe/TicTacToe.jsx
@@ -4,12 +4,14 @@ import './tictactoe.css';
 
 function TicTacToe() {
     return (
-        <div className="game">
+        <>
             <h1>Tic Tac Toe 井字棋遊戲</h1>
-            <div className='game-board'>
-            <Board />
+            <div className="game">
+                <div className='game-board'>
+                <Board />
+                </div>
             </div>
-        </div>
+        </>
     );
 }
 
