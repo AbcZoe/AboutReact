9becb3fd967ee438a7a71f9da4a5bb936227e568
AbcZoe <you@example.com>
2025-06-04 13:42:55 +0800

    完成井字棋，確認按下去後在console.log有提示是按下哪一格

--- component/Tic-Tac-Toe/Board.jsx
+++ component/Tic-Tac-Toe/Board.jsx
@@ -4,22 +4,26 @@ import "./tictactoe.css";
 
 function Board() {
 
+  const handleClick = (i)=>{
+    console.log(`Square[${i}]`)
+  }
+
   return (
     <>
       <div className="board-row">
-        <Square value={0} onSquareClick={() => {}} />
-        <Square value={1} onSquareClick={() => {}} />
-        <Square value={2} onSquareClick={() => {}} />
+        <Square value={0} onSquareClick={() => handleClick(0)} />
+        <Square value={1} onSquareClick={() => handleClick(1)} />
+        <Square value={2} onSquareClick={() => handleClick(2)} />
       </div>
       <div className="board-row">
-        <Square value={3} onSquareClick={() => {}} />
-        <Square value={4} onSquareClick={() => {}} />
-        <Square value={5} onSquareClick={() => {}} />
+        <Square value={3} onSquareClick={() => handleClick(3)} />
+        <Square value={4} onSquareClick={() => handleClick(4)} />
+        <Square value={5} onSquareClick={() => handleClick(5)} />
       </div>
       <div className="board-row">
-        <Square value={6} onSquareClick={() => {}} />
-        <Square value={7} onSquareClick={() => {}} />
-        <Square value={8} onSquareClick={() => {}} />
+        <Square value={6} onSquareClick={() => handleClick(6)} />
+        <Square value={7} onSquareClick={() => handleClick(7)} />
+        <Square value={8} onSquareClick={() => handleClick(8)} />
       </div>
     </>
   );
