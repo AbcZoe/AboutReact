5e52eeecd3e3d438e01fa117c5bb8aff8bc0e0a4
AbcZoe <5b1g0008@stust.edu.tw>
2025-06-12 11:56:59 +0800

    新增凸顯勝利路線的功能

--- component/Tic-Tac-Toe/Board.jsx
+++ component/Tic-Tac-Toe/Board.jsx
@@ -3,6 +3,25 @@ import Square from "./Square";
 import "./tictactoe.css";
 
 function Board({squares, xIsNext, onPlay}) {
+  const winner=calculateWinner(squares);
+  let status;
+  if(winner){
+    status=`Winner: ${winner.winner}`;
+  }else{
+    status=`Next Player: ${xIsNext ? 'X':'O'}`;
+  }
+
+  function renderSquare(i) {
+    const isHighlighted = winner && winner.line.includes(i);
+    return (
+      <Square
+        value={squares[i]}
+        onSquareClick={() => handleClick(i)}
+        highlight={isHighlighted}
+      />
+    );
+  };
+
   function handleClick(i) {
     if (calculateWinner(squares) || squares[i]) {
       return;
@@ -14,14 +33,6 @@ function Board({squares, xIsNext, onPlay}) {
       nextSquares[i] = 'O';
     }
     onPlay(nextSquares);
-  }
-  
-  const winner=calculateWinner(squares);
-  let status;
-  if(winner){
-    status=`Winner: ${winner}`;
-  }else{
-    status=`Next Player: ${xIsNext ? 'X':'O'}`;
   }
 
   function calculateWinner(squares){
@@ -39,7 +50,7 @@ function Board({squares, xIsNext, onPlay}) {
     for (let i=0;i<lines.length;i++){
       const [a,b,c] = lines[i]
       if(squares[a] && squares[a]===squares[b] && squares[a]===squares[c]){
-        return squares[a];
+        return { winner: squares[a], line: [a, b, c] };
       }
     }
     return null;
@@ -50,19 +61,19 @@ function Board({squares, xIsNext, onPlay}) {
       <div className="status">{status}</div>
 
       <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
       <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
       </div>
       <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
       </div>
     </>
   );
--- component/Tic-Tac-Toe/Square.jsx
+++ component/Tic-Tac-Toe/Square.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import { useState } from "react";
 import './tictactoe.css';
 
-function Square({value,onSquareClick}){
-    return(
-        <button className='square' onClick={onSquareClick}>
-            {value}
-        </button>
+function Square({ value, onSquareClick, highlight }) {
+    return (
+      <button
+        className={`square ${highlight ? 'highlight' : ''}`}
+        onClick={onSquareClick}
+      >
+        {value}
+      </button>
     );
-}
+  }
 
 export default Square
